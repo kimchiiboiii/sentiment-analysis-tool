47e21b801e6ff87aab0c36980f93adac520683b3
Matthew <matt.hernz@gmail.com>
2024-06-11 18:26:17 -0400

    api returns with json instead of render template, renamed model.py

--- api.py
+++ api.py
@@ -1,5 +1,5 @@
-from flask import Flask, Blueprint, render_template, request, jsonify
-from model import predict_sentiment, PreprocessingError, PredictionError
+from flask import Flask, Blueprint, request, jsonify
+from sentiment_model import predict_sentiment, PreprocessingError, PredictionError
 
 app = Flask(__name__)
 
@@ -13,7 +13,7 @@ def prediction_api():
     try:
         text = request.form['text']
         sentiment = predict_sentiment(text)
-        return render_template('result.html', sentiment=sentiment)
+        return jsonify({"sentiment": sentiment})
     except PreprocessingError as ppe:
         return jsonify({"error": str(ppe)}), 400
     except PredictionError as pe:
